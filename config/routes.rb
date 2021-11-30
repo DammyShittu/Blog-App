15a959e7200897a6a4750429168d332b257663a1
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-30 16:39:48 +0100

    Add Descriptive README File

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 root 'users#index'
-  resources :users do
-    resources :posts
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
 
 end
