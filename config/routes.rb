1b45f29535d59bccef0b5551a681c67303769002
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-30 15:04:55 +0100

    Generate Posts Controller And Actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'posts/', to: 'posts#index'
+  get 'posts/:id', to: 'posts#show'
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
