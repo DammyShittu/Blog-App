ea02bb5d36928161d86465d7ab3813192b02fa18
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-30 14:57:18 +0100

    Add Placeholders To View Files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index', to: 'users#index'
-  get 'users/show', to: 'users#show'
+
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
