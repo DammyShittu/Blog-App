c8320c198c6ac649413d591c2b8cec0802e88bd9
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-01 16:21:53 +0100

    Re-install Webpacker and Add Tests For Users Index Action

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.5'
 
 gem 'rubocop', '>= 1.0', '< 2.0'
+gem 'rails-controller-testing'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 # Use postgresql as the database for Active Record
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    it 'expects true' do
-      expect(true).to be(true)
+    before(:example) { get users_path }
+
+    it 'checks if action returns correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'checks if action rendered a correct template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'checks if correct placeholder is shown' do
+      expect(response.body).to include('Here is a list of all users')
     end
   end
 end
