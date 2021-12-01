7428b277cf7cef7d04a699add76f019ffa55c61e
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-01 16:35:32 +0100

    Add Tests For Users Show Action

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe "Users", type: :request do
       expect(response.body).to include('Here is a list of all users')
     end
   end
+
+  describe "GET /show" do
+    before(:example) { get user_path(745) }
+
+    it 'checks if action returns correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'checks if action rendered a correct template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'checks if correct placeholder is shown' do
+      expect(response.body).to include('Here is the profile of a particular user')
+    end
+  end
 end
