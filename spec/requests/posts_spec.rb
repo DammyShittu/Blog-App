27e490038423b2824eb96a40780cf78f2c2aa840
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-01 14:47:31 +0100

    Generate Posts Controller Spec

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
+    it 'expects false' do
+      expect(false).to be(false)
+    end
+  end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    it 'expects false' do
+    it 'expects true' do
       expect(true).to be(true)
     end
   end
