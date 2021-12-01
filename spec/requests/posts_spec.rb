e4774e0b3dac13cf58d21662c0cbfcacb0616abe
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-01 16:57:36 +0100

    Add Tests For Posts Show Action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe "Posts", type: :request do
       expect(response.body).to include('Here is the list of posts for a particular user')
     end
   end
+
+  describe "GET /show" do
+    before(:example) { get user_post_path(13, 45) }
+
+    it 'checks if action returns correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'checks if action rendered a correct template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'checks if correct placeholder is shown' do
+      expect(response.body).to include('Here is one of the posts written by a user')
+    end
+  end
 end
