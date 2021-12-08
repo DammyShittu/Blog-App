67052ee471824c8353f1bfce22326259a4673a3a
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-08 15:08:18 +0100

    Update Schema File And Populate Data In Seed File

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_080550) do
+ActiveRecord::Schema.define(version: 2021_12_08_101853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,13 +25,13 @@ ActiveRecord::Schema.define(version: 2021_12_07_080550) do
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  create_table "likes", id: false, force: :cascade do |t|
-    t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
+  create_table "likes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_likes_on_author_id", unique: true
-    t.index ["post_id"], name: "index_likes_on_post_id", unique: true
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -56,5 +56,7 @@ ActiveRecord::Schema.define(version: 2021_12_07_080550) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
