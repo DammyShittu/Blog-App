da975101cd10aad3ffa0fc37b9e34938178c7c27
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-09 13:19:10 +0100

    Write Code For New And Create Methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,27 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @current = current_user
+  end
+
+    def create
+      new_post = current_user.posts.build(post_params)
+
+      respond_to do |format|
+        format.html do
+          if new_post.save
+            redirect_to user_post_path(new_post.author_id, new_post.id), notice: "Post has been successfully created!"
+          else
+            render :new, alert: "Post not created. Please try again!"
+          end
+        end
+      end
+    end
+
+    private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 end
