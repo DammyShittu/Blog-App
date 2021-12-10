2a6c7cf4cd735507c8443461876475b61819b905
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-10 13:03:37 +0100

    Add Counter Cache To Solve N + 1 Problem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :post, counter_cache: :comments_counter
 
   def update_comments_counter
     post.increment!(:comments_counter)
