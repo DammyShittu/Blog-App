1666c33924dc90b5eb659cb0d65fa2a424da0d22
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-14 00:10:29 +0100

    Change Root Path To Sign In Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-root 'users#index'
+
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show]
   end
