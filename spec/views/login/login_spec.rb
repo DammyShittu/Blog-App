22f26ee7678ed19ac6ef43d09c929395f8c4171c
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-16 00:23:55 +0100

    Add More Tests For Login Page

--- spec/views/login/login_spec.rb
+++ spec/views/login/login_spec.rb
@@ -1,44 +1,43 @@
 require 'rails_helper'
 
-RSpec.feature "Sessions Page" do
-  feature "shows the right fields" do
-    background { visit new_user_session_path }
-
-    scenario "displays email field" do
-      expect(page).to have_field('user[email]')
-    end
-
-    scenario "displays password field" do
-      expect(page).to have_field('user[password]')
-    end
-
-    scenario "displays email field" do
-      expect(page).to have_button('Log in')
-    end
+RSpec.feature 'Logins', type: :feature do
+  background { visit new_user_session_path }
+  scenario 'displays email field' do
+    expect(page).to have_field('user[email]')
   end
 
-  # feature "check wrong submissions" do
-    scenario 'submit form without email and password' do
+  scenario 'displays password field' do
+    expect(page).to have_field('user[password]')
+  end
+
+  scenario 'displays email field' do
+    expect(page).to have_button('Log in')
+  end
+
+  context 'Form Submission' do
+    scenario 'Submit form without email and password' do
       click_button 'Log in'
       expect(page).to have_content 'Invalid Email or password.'
     end
 
-    scenario 'submit form with incorrect email and password' do
-      fill_in 'Email', with: 'jkfirst@yahoo.com'
-      fill_in 'Password', with: '222222'
+    scenario 'Submit form with incorrect email and password' do
+      within 'form' do
+        fill_in 'Email', with: 'mymail@gmail.com'
+        fill_in 'Password', with: '222222'
+      end
       click_button 'Log in'
       expect(page).to have_content 'Invalid Email or password.'
     end
-  end
 
-  feature "redirect to right path after authentication" do
-    scenario "Path should change on sucessful login" do
-      @user = User.create(name: "Adedamola Shittu", bio: "Software Developer", email: "johndoe24@gmail.com", password: "password", confirmed_at: Time.now)
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
+    scenario 'Submit form with correct email and password' do
+      @user = User.create(name: 'Adedamola', email: 'admin@gmail.com', password: 'password', confirmed_at: Time.now)
+      within 'form' do
+        fill_in 'Email', with: @user.email
+        fill_in 'Password', with: @user.password
+      end
       click_button 'Log in'
-
-    expect(page).to have_current_path(authenticated_root_path)
-  end
+      expect(page).to have_current_path(authenticated_root_path)
+      expect(page).to have_content 'Signed in successfully.'
+    end
   end
 end
