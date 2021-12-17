45fa3ba78764370fdf4016521e01e5f008dd7fa6
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-17 13:50:16 +0100

    Fix Rubocop Linter Errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
-
 end
