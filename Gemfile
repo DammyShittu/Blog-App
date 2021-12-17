ea6dac2c617a2b35351cdce15b06396cbb3bd995
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-12-17 13:41:55 +0100

    Add Authentication For Login And Signup With JWT

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.5'
 
+gem 'devise-jwt'
 # Give authorization to users
 gem 'cancancan'
 # Use devise for authentication
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  protect_from_forgery prepend: true
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+
 end
